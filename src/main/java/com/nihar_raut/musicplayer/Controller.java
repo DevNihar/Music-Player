72ed0ddc41f80f148ee266e19374f291c2eaff9e
Nihar Raut <niharraut.prog@gmail.com>
2022-10-02 23:32:51 +0530

    initial commit

---
+++ src/main/java/com/nihar_raut/musicplayer/Controller.java
@@ -0,0 +1,7 @@
+package com.nihar_raut.musicplayer;
+
+import javafx.scene.media.Media;
+
+public class Controller {
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.nihar_raut.musicplayer {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.nihar_raut.musicplayer to javafx.fxml;
+    exports com.nihar_raut.musicplayer;
+}
