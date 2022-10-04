88acbb3f06461c594f232f93aae1cb6025a630a0
DevNihar <110764445+DevNihar@users.noreply.github.com>
2022-10-05 02:31:40 +0530

    added funtion to create the song list and and initializing the Song list as well as set up the cell factories for the table columns

--- src/main/java/com/nihar_raut/musicplayer/Controller.java
+++ src/main/java/com/nihar_raut/musicplayer/Controller.java
@@ -3,20 +3,16 @@ package com.nihar_raut.musicplayer;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
-
 import DataModel.Song;
 import com.mpatric.mp3agic.*;
-import javafx.beans.InvalidationListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
-import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Callback;
 import org.kordamp.ikonli.javafx.FontIcon;
@@ -48,6 +44,13 @@ public class Controller {
         songs = FXCollections.observableArrayList();
         players = new ArrayList<>();
         dirPath = "D:\\Projects\\JavaSoftwareProjects\\MusicPlayer\\MusicLibrary";
+        loadSongs();
+        nameColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Song, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Song, String> songStringCellDataFeatures) {
+                return songStringCellDataFeatures.getValue().songNameProperty();
+            }
+        });
 
         nameColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Song, String>, ObservableValue<String>>() {
             @Override
@@ -55,24 +58,18 @@ public class Controller {
                 return songStringCellDataFeatures.getValue().songNameProperty();
             }
         });
-        String file = "MusicLibrary\\Ed Sheeran - Perfect 320kbps(Mobmirchi.in).mp3";
-
-        File audioFile = new File(file);
-        try {
-            Mp3File mp3File =new Mp3File(audioFile.getPath());
-            ID3v2 tag = mp3File.getId3v2Tag();
-            Song newSong = new Song("0", tag.getTitle(),
-                    formatDuration(mp3File.getLengthInSeconds()),
-                    tag.getArtist(),
-                    tag.getAlbum(),
-                    tag.getUrl());
-        }catch (IOException e){
-            e.printStackTrace();
-        }catch (UnsupportedTagException e){
-            System.out.println("unsupported tag");
-        }catch (InvalidDataException e){
-            System.out.println("Invalid Data");
-        }
+        durationColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Song, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Song, String> songStringCellDataFeatures) {
+                return songStringCellDataFeatures.getValue().durationProperty();
+            }
+        });
+        artistColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Song, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Song, String> songStringCellDataFeatures) {
+                return songStringCellDataFeatures.getValue().artistNameProperty();
+            }
+        });
         songTableView.setItems(songs);
 //        String file = "MusicLibrary\\Ed Sheeran - Perfect 320kbps(Mobmirchi.in).mp3";
 //
@@ -98,6 +95,43 @@ public class Controller {
 //        player.play();
     }
 
+    public void loadSongs(){
+        File directory = new File(dirPath);
+        File[] files =  directory.listFiles();
+        int id = 0;
+        for(File file: files){
+            String name = file.getName();
+            System.out.println("Loading: " + name);
+//            if(name.endsWith("m4a")){
+//                file.delete();
+//            }
+            if(name.endsWith("mp3") || name.endsWith("wav")){
+
+                try {
+                    Mp3File mp3File =new Mp3File(file.getAbsolutePath());
+                    ID3v2 tag = mp3File.getId3v2Tag();
+                    if(tag == null){
+                        continue;
+                    }
+                    id++;
+                    System.out.println(id);
+                    Song newSong = new Song(String.valueOf(id), tag.getTitle()!=null ? tag.getTitle() : "-",
+                            formatDuration(mp3File.getLengthInSeconds()),
+                            tag.getArtist()!=null ? tag.getArtist(): "-",
+                            tag.getAlbum()!=null ? tag.getAlbum() : "-",
+                            tag.getUrl());
+                    songs.add(newSong);
+                }catch (IOException e){
+                    e.printStackTrace();
+                }catch (UnsupportedTagException e){
+                    System.out.println("unsupported tag");
+                }catch (InvalidDataException e){
+                    System.out.println("Invalid Data");
+                }
+            }
+        }
+    }
+
     @FXML
     public void handlePlayPause(){
         if(play_pause_icon.getIconLiteral().equals("cil-media-play")){
@@ -120,14 +154,14 @@ public class Controller {
             stringBuilder.append(":");
             stringBuilder.append("0");
             stringBuilder.append(seconds%60);
-            System.out.println(stringBuilder);
+//            System.out.println(stringBuilder);
             return stringBuilder.toString();
         }else {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append(seconds/60);
             stringBuilder.append(":");
             stringBuilder.append(seconds%60);
-            System.out.println(stringBuilder);
+//            System.out.println(stringBuilder);
             return stringBuilder.toString();
         }
     }
